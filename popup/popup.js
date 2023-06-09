a30bed90317746101556fbf78933d4f08a283c6f
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2023-06-09 23:34:27 +0530

    created react app

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,21 @@
-$("#home").click(function (){
-    console.log("redirect to home page");
-    chrome.tabs.create({url:'https://snip-ninja.netlify.app/'});
-})
+import { get } from 'axios/dist/browser/axios.cjs';
+
+$("#home").click(function () {
+  console.log("redirect to home page");
+  chrome.tabs.create({ url: "https://snip-ninja.netlify.app/" });
+});
+
+$(".btn").click(() => {
+  // localStorage.setItem("ext-id", "snip ninja extension id 007");
+  console.log("btn clicked");
+  getUsers();
+});
+
+const getUsers = () => {
+  get("https://reqres.in/api/users")
+    .then((response) => {
+      const users = response.data.data;
+      console.log(`GET users`, users);
+    })
+    .catch((error) => console.error(error));
+};
