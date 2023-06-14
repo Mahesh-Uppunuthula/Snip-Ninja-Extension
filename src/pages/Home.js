6cf02b3dfa93f547f45525ff46c86fbc1810dd26
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2023-06-15 01:23:29 +0530

    added editor to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Axios from "axios";
+
+// EDITOR
+import { Editor } from "@monaco-editor/react";
+
+// ICONS
 import logoutIcon from "../assets/logout.svg";
 
 function Home() {
@@ -7,10 +12,33 @@ function Home() {
 
   const BASE_URL = "https://snippets-sever.onrender.com/";
 
+  const options = {
+    autoIndent: 'full',
+    contextmenu: true,
+    fontFamily: 'monospace',
+    fontSize: 13,
+    lineHeight: 24,
+    hideCursorInOverviewRuler: true,
+    matchBrackets: 'always',
+    minimap: {
+      enabled: true,
+    },
+    scrollbar: {
+      horizontalSliderSize: 3,
+      verticalSliderSize: 3,
+    },
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: 'line',
+    automaticLayout: true,
+  }; 
+
+
   function handleSubmit() {
     console.log("request to save file");
     const url = BASE_URL + "dashboard";
-    
+
     Axios.get(url, {
       headers: {
         Authorization: token,
@@ -24,16 +52,30 @@ function Home() {
       });
   }
 
-  function logUserOut(){
+  function logUserOut() {
     console.log("clicked logout");
     window.localStorage.removeItem("token");
   }
 
+  function handleOnCodeChange(value, event) {}
+
   return (
     <>
-      <div className="container">
-        <div className="home-container">
-          <div className="top-container">
+      <div className="home-container">
+        <Editor
+          className="editor"
+          width="300px"
+          height="300px"
+          defaultValue={"//this is a comment"}
+          // defaultLanguage="typescript && javascript && css && less && scss && json && html"
+          defaultLanguage="javascript"
+          theme="vs-dark"
+          options={options}
+          onChange={(value, event) => {
+            handleOnCodeChange(value, event);
+          }}
+        />
+        {/* <div className="top-container">
             <div id="home" className="brand emphasis-text link-item">
               <a href="https://snip-ninja.netlify.app"></a>
             </div>
@@ -58,8 +100,7 @@ function Home() {
             <button className="btn" type="submit" onClick={handleSubmit}>
               Save
             </button>
-          </div>
-        </div>
+          </div> */}
       </div>
     </>
   );
