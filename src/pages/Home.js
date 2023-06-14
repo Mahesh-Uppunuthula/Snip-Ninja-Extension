79df31f3bba58099e5a0998d54d64c977b72ff01
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2023-06-15 03:44:01 +0530

    added home page components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Axios from "axios";
 
+// LINKS
+import { HOME_URL, BASE_URL } from "../services/helper";
+
 // EDITOR
 import { Editor } from "@monaco-editor/react";
 
 // ICONS
+import fileIcon from "../assets/file.svg";
+import buldIcon from "../assets/bulb.svg";
 import logoutIcon from "../assets/logout.svg";
 
 function Home() {
@@ -13,13 +18,13 @@ function Home() {
   const BASE_URL = "https://snippets-sever.onrender.com/";
 
   const options = {
-    autoIndent: 'full',
+    autoIndent: "full",
     contextmenu: true,
-    fontFamily: 'monospace',
+    fontFamily: "monospace",
     fontSize: 13,
     lineHeight: 24,
     hideCursorInOverviewRuler: true,
-    matchBrackets: 'always',
+    matchBrackets: "always",
     minimap: {
       enabled: true,
     },
@@ -30,10 +35,11 @@ function Home() {
     selectOnLineNumbers: true,
     roundedSelection: false,
     readOnly: false,
-    cursorStyle: 'line',
+    cursorStyle: "line",
     automaticLayout: true,
-  }; 
+  };
 
+  let ediorTip = "paste your code below";
 
   function handleSubmit() {
     console.log("request to save file");
@@ -59,48 +65,65 @@ function Home() {
 
   function handleOnCodeChange(value, event) {}
 
+  function redirectToSnipNinjaPage() {
+    const homePageUrl = HOME_URL;
+    window.open(homePageUrl, "_blank");
+  }
+
   return (
     <>
       <div className="home-container">
-        <Editor
-          className="editor"
-          width="300px"
-          height="300px"
-          defaultValue={"//this is a comment"}
-          // defaultLanguage="typescript && javascript && css && less && scss && json && html"
-          defaultLanguage="javascript"
-          theme="vs-dark"
-          options={options}
-          onChange={(value, event) => {
-            handleOnCodeChange(value, event);
-          }}
-        />
-        {/* <div className="top-container">
-            <div id="home" className="brand emphasis-text link-item">
-              <a href="https://snip-ninja.netlify.app"></a>
+        <div className="lg-inner-cont">
+          <div className="home-top-container">
+            <div
+              className="brand emphasis-text link-item"
+              onClick={redirectToSnipNinjaPage}
+            >
+              Snip Ninja
             </div>
-            <div className="img__wrap" onClick={logUserOut}>
-              <img className="logout-icon" src={logoutIcon}></img>
-              <p className="img-desc">logout</p>
+            <div className="icon-bg">
+              <img className="hm-icon" src={logoutIcon} />
             </div>
           </div>
-          <div className="form">
-            <div className="heading">
-              Create a new snippet from highlighted code
+          <div className="editor-container">
+            <div className="editor-top-pane">
+              <div className="window-controls">
+                <div className="control red"></div>
+                <div className="control yellow"></div>
+                <div className="control green"></div>
+              </div>
+              <div className="tip-container">
+                <img className="hm-icon" src={buldIcon} alt="tip icon" />
+                <p className="tip">{ediorTip}</p>
+              </div>
             </div>
-            <div>
-              <input
-                className="input"
-                type="text"
-                name="file"
-                placeholder="enter file name here.."
-                autofocus="true"
-              ></input>
+            <Editor
+              className="editor"
+              width="100%"
+              height="300px"
+              defaultValue={"//this is a comment"}
+              // defaultLanguage="typescript && javascript && css && less && scss && json && html"
+              defaultLanguage="javascript"
+              theme="vs-dark"
+              options={options}
+              onChange={(value, event) => {
+                handleOnCodeChange(value, event);
+              }}
+            />
+
+            <div className="file-container">
+              <div className="file-input-cont">
+                <div className="left">
+                  <img className="hm-icon" src={fileIcon} alt="file icon" />
+                  <input type="textx" placeholder="enter file name" className="file-input" />
+                </div>
+                <div className="right">
+                  <button className="file-btn">save</button>
+                </div>
+              </div>
             </div>
-            <button className="btn" type="submit" onClick={handleSubmit}>
-              Save
-            </button>
-          </div> */}
+          </div>
+        </div>
       </div>
     </>
   );
